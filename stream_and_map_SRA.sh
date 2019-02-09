f19b5e18f9daf18907a90107433069bad080971a
Taylor O'Connell <t89oconnell@gmail.com>
2019-02-08 17:17:42 -0800

    Add comment section

--- stream_and_map_SRA.sh
+++ stream_and_map_SRA.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+#This script streams FASTQ data from the Sequence Read Archive for a user-specified SRA run ID
+#and maps the reads against a user-specified database using bowtie2
+
 SRA_ID=$1
 
-fastq-dump --split-spot -Z --readids --skip-technical --read-filter pass --dumpbase --clip ${SRA_ID} | bowtie2 -x all_MarRef_prophages --interleaved - --xeq --no-unal -S ${SRA_ID}.sam
+fastq-dump --split-spot -Z --readids --skip-technical --read-filter pass --dumpbase --clip ${SRA_ID} | \
+bowtie2 -x all_MarRef_prophages --interleaved - --xeq --no-unal -S ${SRA_ID}.sam
